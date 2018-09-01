fe91846b1e9b0c27a11b97962991bee4da66ff9e
Daniel Ecer <de-code@users.noreply.github.com>
2018-09-01 20:01:22 +0100

    added notebook comparing methods to balance the athar dataset

--- citation_sentiment_analysis/utils/scoring.py
+++ citation_sentiment_analysis/utils/scoring.py
@@ -5,14 +5,14 @@ from sklearn.metrics.scorer import check_scoring
 
 # similar to cross_val_score but only checks one split
 def train_test_score(
-    model, X, y, train_test_ratio=0.25, scoring='accuracy', shuffle=True, random_state=None,
+    model, X, y, test_size=0.25, scoring='accuracy', shuffle=True, random_state=None,
     train_test_split_fn=None):
 
     if train_test_split_fn is None:
         train_test_split_fn = train_test_split
     scorer = check_scoring(model, scoring)
     X_train, X_test, y_train, y_test = train_test_split_fn(
-        X, y, test_size=train_test_ratio, shuffle=shuffle, random_state=random_state
+        X, y, test_size=test_size, shuffle=shuffle, random_state=random_state
     )
     model.fit(X_train, y_train)
     return scorer(model, X_test, y_test)
