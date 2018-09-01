f188764c23ac2c4ad74e9e67d8e22be0e957c888
Daniel Ecer <de-code@users.noreply.github.com>
2018-09-01 19:26:19 +0100

    removed print statements

--- citation_sentiment_analysis/utils/scoring.py
+++ citation_sentiment_analysis/utils/scoring.py
@@ -14,8 +14,5 @@ def train_test_score(
     X_train, X_test, y_train, y_test = train_test_split_fn(
         X, y, test_size=train_test_ratio, shuffle=shuffle, random_state=random_state
     )
-    print('shapes:', [x.shape for x in [X_train, X_test, y_train, y_test]])
-    print('y_train:', pd.Series(y_train).value_counts().to_dict())
-    print('y_test:', pd.Series(y_test).value_counts().to_dict())
     model.fit(X_train, y_train)
     return scorer(model, X_test, y_test)
